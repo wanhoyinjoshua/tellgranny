b19469e3022df8b48a4ddd7c2706c638312012b1
joshua <wanhoyinsjoahu@yahoo.com>
2021-07-18 19:33:17 +1000

    hi

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express")
+const mongoose = require('mongoose');
+
+const app= express();
+const PORT=process.env.PORT||4000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+
+  
+app.listen(PORT, ()=>{console.log("server running")});
+
+
+app.get("/",(req,res,next)=>{
+    //get form 
+    res.sendFile('index.html', { root: __dirname });
+})
+
+app.post("/submitquestion",(req,res,next)=>{
+
+    const conn = "mongodb+srv://wanhoyinjoshua:8sr^3B=81@cluster0.zccc4.mongodb.net/grannytell?retryWrites=true&w=majority";
+const connection = mongoose.createConnection(conn, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
+const UserSchema = new mongoose.Schema({
+    question:String,
+    emial:String
+});
+
+
+const User = connection.model('questions', UserSchema);
+
+const newquestion = new User({
+    question: req.body.question,
+    email: req.body.body.email
+    
+});
+
+newquestion.save()
+.then((user) => {
+    res.sendFile('sucees.html', { root: __dirname });
+})
+ 
+
+   
+  
+
+
+
+    //get form 
+   
+})
